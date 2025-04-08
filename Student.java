2ae25d75dee6c1908c394330b28e26abae984222
Aman Kumar 12955 <22bcs12955@cuchd.in>
2025-04-08 20:37:33 +0530

    Create  Student.java

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+    private String name;
+    private Course course;
+
+    public Student(String name, Course course) {
+        this.name = name;
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [Name=" + name + ", Course=" + course.getCourseName() + ", Duration=" + course.getDuration() + " months]";
+    }
+}
