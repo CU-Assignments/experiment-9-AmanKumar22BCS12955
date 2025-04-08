f4bf6663d7b146702eac0918451bbcac85945dd6
Aman Kumar 12955 <22bcs12955@cuchd.in>
2025-04-08 20:55:23 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,35 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    private int accountId;
+    private String accountHolder;
+    private double balance;
+
+    // Getters and Setters
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
